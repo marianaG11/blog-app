5e69274027bc8201c1bdd2734604e4f84e2b828c
marianaG11 <mariana.garcia.16@icloud.com>
2022-06-19 23:23:31 -0500

    display blogs on home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,27 @@
+import { useState } from "react"; //to create a reactive value
+
+
 const Home = () => {
+    const  [blogs, setBlogs] = useState([
+        {title: 'blog1', body: 'lorem ipsum...', author: 'Mariana', id: 1},
+        {title: 'blog2', body: 'lorem ipsum...', author: 'Mariana', id: 2},
+        {title: 'blog3', body: 'lorem ipsum...', author: 'Mariana', id: 3}
+    ]);
+
+
+
+
+//map through the array of objects
+//need the key attribute to keep track of each item in the dom as React outputs it
+    //the id prop; must be unique
     return (
         <div className="home">
-            <h2>Homepage</h2>
+           {blogs.map((blog) => (
+               <div className="blog-preview" key={blog.id}>
+                   <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+               </div>
+            ))}
         </div>
      );
 }
