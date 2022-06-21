eb79813313a154c4d3fa234978a23ff0fd64dcb7
marianaG11 <mariana.garcia.16@icloud.com>
2022-06-21 03:15:42 -0500

    Create component

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,15 +1,24 @@
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
-
+import useFetch from "./useFetch";
 const BlogDetails = () => {
+     //can access the id param
     const {id} = useParams();   //allows to grab parameters from the Routes
-    //can access the id param
+    const {data: blog, loading, error} = useFetch(`http://localhost:8000/blogs/${id}`);
     return (
         <>
-        <Navbar />
-        <div className="blog-details">
-            <h2>Blog Details {id} </h2>
-        </div>
+            <Navbar />
+            <div className="blog-details">
+                {loading && <div>Loading...</div>}
+                {error && <div>{error}</div>}
+                {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+                )}
+            </div>
         </>
     );
 }
--- src/Create.js
+++ src/Create.js
@@ -1,12 +1,48 @@
 import Navbar from "./Navbar";
+import { useState } from "react";
 
 const Create = () => {
+
+    const [title, setTitle] = useState(''); //when user types in, setTitle gets called and updates
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mariana');
+    
+    //the value being typed in
+    //e.target is the input element; can access the value with e.target.value
+
     return (
         <>
         <Navbar />
         <div className="create">
             <h2>Add a new blog</h2>
-
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value = {title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Content:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)} 
+                ></textarea>
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                
+                >
+                    <option value="Mariana">Mariana</option>
+                    <option value="Maggie">Maggie</option>
+                </select>
+                <button>Add Blog</button>
+               <p>{title}</p>
+               <p>{body}</p>
+               <p>{author}</p>
+            </form>
         </div>
         </>
     )
